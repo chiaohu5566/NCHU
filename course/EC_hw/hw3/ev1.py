e5ec57df3501d7ff2f85fd52b1c8fb39284dc511
chiaohu5566 <chiaohu5566@gmail.com>
2021-04-08 01:50:03 +0800

    ec_hw3

--- course/EC_hw/hw3/ev1.py
+++ course/EC_hw/hw3/ev1.py
@@ -13,8 +13,35 @@ import sys
 import yaml
 import math
 from random import Random
-from ev1_plot_utils import *
 
+pause_length=1
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(1,1)
+plt.ion()
+plt.show()
+
+def plotFitnessFunc(fitnessFunc,minLimit,maxLimit,numPoints):
+    step=(maxLimit-minLimit)/numPoints
+    y=[]
+    xr=[step*i+minLimit for i in range(numPoints)]
+    for x in xr:
+        y.append(fitnessFunc(x))
+    plt.plot(xr,y,linestyle='solid')    
+    return plt
+
+
+def plotGeneration(pop):
+    mpl_pts=[]
+    for p in pop:
+        mpl_pt=ax.scatter(p.x, p.fit)
+        mpl_pts.append(mpl_pt)
+    plt.pause(pause_length)
+    return mpl_pts
+
+def removePlotGeneration(mpl_pts):        
+    for pt in mpl_pts:
+        pt.remove()            
 
 #EV1 Config class 
 class EV1_Config:
